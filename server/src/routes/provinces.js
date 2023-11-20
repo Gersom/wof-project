c7fd991f9e01efa46bc275545ec01183fd7862c5
DaroOps <romerosd089@hotmail.com>
2023-11-20 12:36:07 -0500

    [UPDATE] re-named handlers for controllers
    Controllers are responsible for buildind a response for the  client

--- server/src/routes/provinces.js
+++ server/src/routes/provinces.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const handlers = require("../handlers/provinces")
+const handlers = require("../controllers/provinces")
 
 // route => /breeds/...
 
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,17 +1,17 @@
 const express = require("express")
 const router = express.Router()
-const handlers = require("../handlers/users")
+const controllers = require("../controllers/users")
 
 // route => /breeds/...
 
-router.get("/", handlers.getAllUsers)
+router.get("/", controllers.getAllUsers)
 
-router.get("/:id", handlers.getUser)
+router.get("/:id", controllers.getUser)
 
-router.post("/", handlers.createUser)
+router.post("/", controllers.createUser)
 
-router.put("/:id", handlers.updateUser)
+router.put("/:id", controllers.updateUser)
 
-router.delete("/:id", handlers.deleteUser)
+router.delete("/:id", controllers.deleteUser)
 
 module.exports = router
