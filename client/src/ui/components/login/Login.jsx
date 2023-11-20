d0b3aa9630cf1f4ba79cbed1cc6e9eb59d24b0ff
alejoo61 <alejtk61@gmail.com>
2023-11-20 18:37:19 -0300

    estilos para login

--- client/src/ui/components/login/Login.jsx
+++ client/src/ui/components/login/Login.jsx
@@ -1,22 +1,47 @@
-import logo from "@images/landing/logo.svg";
+import { Link } from "react-router-dom";
+import routerNames from "@src/common/constants/routes";
+import logo from "@icons/nav/logo.svg";
+import password from "@icons/password.svg";
+import email from "@icons/email.svg";
 import styles from "./styles.module.scss";
 
 const Login = () => {
   return (
     <>
-      <div className={styles.home}>
-        <img className={styles.image} src={logo} alt="Logo" />
-        <h1>Iniciar Sesion</h1>
-        <form action="">
-          <label htmlFor="email">Email: </label>
-          <input type="text" name="email" />
+      <div className={styles.container}>
+        <div className={styles["form-container"]}>
+          <img src={logo} alt="Logo" />
+          <div>
+            <h1>Iniciar Sesion</h1>
+            <hr />
+            <br />
+          </div>
+
+          <div>
+            <form action="">
+              <label htmlFor="email">
+                <img src={email} alt="" /> Email:
+              </label>
+              <input type="text" name="email" />
+              <br />
+              <label htmlFor="password">
+                <img src={password} alt="" /> Contraseña:
+              </label>
+              <input type="password" name="password" />
+              <br />
+              <button type="submit">Ingresar</button>
+              <Link to={routerNames["formRegister"]}>
+                <button>Registrarse</button>
+              </Link>
+            </form>
+          </div>
+
           <br />
-          <label htmlFor="password">Contraseña: </label>
-          <input type="password" name="password" />
-          <br />
-          <button type="submit">Enviar</button>
-          <button type="submit">Registrarse</button>
-        </form>
+          <hr />
+        </div>
+        <div>
+          <p>By Gerson</p>
+        </div>
       </div>
     </>
   );
