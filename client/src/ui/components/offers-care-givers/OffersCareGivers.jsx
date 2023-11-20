a4a8801f5d0a46bdb0b52f125fd4644e4d692ac9
EseToni <falsigfx@gmail.com>
2023-11-20 16:07:37 +0100

    [UPDATE] styles offers care givers

--- client/src/ui/components/offers-care-givers/OffersCareGivers.jsx
+++ client/src/ui/components/offers-care-givers/OffersCareGivers.jsx
@@ -6,6 +6,7 @@ import CardInfo from '../card-info/CardInfo';
 
 const OffersCareGivers = () => {
     const [offers, setOffers] = useState([]);
+
     useEffect(() => {
         const getOffers = async () => {
             const offers = await getOffersCareGivers();
@@ -13,7 +14,7 @@ const OffersCareGivers = () => {
         };
         getOffers();
     }, []);
-    console.log(offers);
+
 	return (
 		<div className={styles.containerMainGrid}>
 			<div className={styles.containerCardInfo}>
@@ -21,7 +22,7 @@ const OffersCareGivers = () => {
 				<CardInfo />
 			</div>
 			<div className={styles.containerOffers}>
-				{offers.slice(0,1).map((offer,index) => (
+				{offers.slice(0,3).map((offer,index) => (
                     <CardOffersCaregivers
                         key={index}
                         name={offer.name}
