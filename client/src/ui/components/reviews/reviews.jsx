fd7527a2aceeff3ec2fccda5e976bcaf91d02714
Abril31 <brivaldi0416@gmail.com>
2023-11-21 02:40:26 -0400

    componente reviews creado

--- client/src/ui/components/reviews/reviews.jsx
+++ client/src/ui/components/reviews/reviews.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Reviews = () => {
-  return <div>reviews</div>;
+  return (
+    <div>
+      <form>
+        <h3>Deja tu reseña</h3>
+        <h4>Puntuación:⭐⭐⭐⭐⭐</h4>
+        <textarea
+          placeholder="Escribe tu reseña..."
+          cols="15"
+          rows="10"
+        ></textarea>
+        <button>Cancelar</button>
+        <button type="submit">Publicar</button>
+      </form>
+    </div>
+  );
 };
 
 export default Reviews;
