968823a929fdc9ab0f1c9af92055134ac3fc14ad
Abril31 <brivaldi0416@gmail.com>
2023-11-22 11:23:31 -0400

    Avance formulario de reseña

--- client/src/ui/components/reviews/FormReview.jsx
+++ client/src/ui/components/reviews/FormReview.jsx
@@ -1,20 +1,21 @@
 import styles from "./styles.module.css";
 import React from "react";
-import { MdStarOutline } from "react-icons/md";
+import { FaStar } from "react-icons/fa";
 import { useState } from "react";
 
 const FormReview = () => {
-  const [rating, setRating] = useState(null);
+  const [rating, setRating] = useState(0);
   const [state, setState] = useState({
     name: "",
-    rating: 0,
+    rating: 1,
     review: "",
   });
+  console.log(state);
   const renderStars = () => {
     const stars = [];
     for (let i = 1; i <= 5; i++) {
       stars.push(
-        <MdStarOutline
+        <FaStar
           key={i}
           onClick={() => setRating(i)}
           style={{ color: i <= rating ? "gold" : "gray", cursor: "pointer" }}
@@ -26,29 +27,40 @@ const FormReview = () => {
   const handleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
+
+    setState((emptyState) => ({
+      ...emptyState,
+      [name]: value,
+    }));
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    //dispatch postReview(state);
+    //ruta ¿postReview(state)?;
   };
 
   return (
     <div className={styles.cont}>
       <form onSubmit={handleSubmit}>
         <h3>Deja tu reseña</h3>
-        <label>Nombre</label>
+        <div className={styles.divider}></div>
+        <label>Nombre:</label>
         <input onChange={handleChange} type="text" name="name" />
         <h4>Puntuación: {renderStars()}</h4>
         <textarea
+          onChange={handleChange}
           placeholder="Escribe tu reseña..."
           cols="15"
           rows="10"
+          name="review"
         ></textarea>
-        <button className={styles.btnCancel}>Cancelar</button>
-        <button className={styles.btnSubmit} type="submit">
-          Publicar
-        </button>
+        <div className={styles.btnCont}>
+          <button className={styles.btnCancel}>Cancelar</button>
+          <div className={styles.separator}></div>
+          <button className={styles.btnSubmit} type="submit">
+            Publicar
+          </button>
+        </div>
       </form>
     </div>
   );
