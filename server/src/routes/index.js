78a26b4aa6d4bd774bf8bc9be553a3d9b363ad2f
JBartolo <jesusbartolojbh@gmail.com>
2023-11-21 20:25:03 -0500

    [ADD] routes/owners & controllers/owners & handlers/owners

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -17,5 +17,6 @@ const router = express.Router()
 router.use(`/users`, require(`./users`))
 router.use(`/provinces`, require(`./provinces`))
 router.use(`/caregivers`, require(`./caregivers`))
+router.use(`/owners`, require(`./owners`))
 
 module.exports = router
