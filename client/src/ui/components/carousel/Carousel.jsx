31c8c65716fd37a06c5b513796b9e4409e7c90ad
EseToni <falsigfx@gmail.com>
2023-11-22 22:02:58 +0100

    [ADD] component/ Carousel

--- client/src/ui/components/carousel/Carousel.jsx
+++ client/src/ui/components/carousel/Carousel.jsx
@@ -1,9 +1,28 @@
 import styles from './styles.module.scss';
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 function Carousel() {
-  return (
-    <div>Carousel</div>
-  )
+	return (
+		<Swiper
+			spaceBetween={50}
+			slidesPerView={3}
+		>
+			<SwiperSlide>
+				<img
+					src='https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg'
+					style={{ width: '30px', height: '30px' }}
+				/>
+			</SwiperSlide>
+            <SwiperSlide>
+				<img
+					src='https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg'
+					style={{ width: '30px', height: '30px' }}
+				/>
+			</SwiperSlide>
+            
+            
+		</Swiper>
+	);
 }
 
-export default Carousel
+export default Carousel;
