2de0a7043ec27d1844fc176eb95847a2b91c398a
Abril31 <brivaldi0416@gmail.com>
2023-11-23 16:55:25 -0400

    Componente CardReviwPet creado

--- client/src/ui/pages/details/Details.jsx
+++ client/src/ui/pages/details/Details.jsx
@@ -1,41 +1,43 @@
-import styles from './styles.module.scss';
-import CardUser from '@src/ui/components/card-user/CardUser';
-import Carousel from '@src/ui/components/carousel/Carousel';
-import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import useGetDetails from '@src/common/hooks/useGetDetails';
-import CardInfoPet from '@src/ui/components/card-info/CardInfoPet';
+import styles from "./styles.module.scss";
+import CardUser from "@src/ui/components/card-user/CardUser";
+import Carousel from "@src/ui/components/carousel/Carousel";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import useGetDetails from "@src/common/hooks/useGetDetails";
+import CardInfoPet from "@src/ui/components/card-info/CardInfoPet";
 
 const Details = () => {
-	const { id } = useParams();
-	const { isLoading, details } = useGetDetails(id);
+  const { id } = useParams();
+  const { isLoading, details } = useGetDetails(id);
 
-	return (
-		<div className={styles.mainContainerGrid}>
-			<div className={styles.containerLeft}>
-				{!isLoading && (
-					<CardUser
-						name={details.owner.name}
-						role={details.owner.role}
-						address={details.owner.address}
-						rating={details.owner.rating}
-						imgSrc={details.owner.profilePicture}
-						cellPhone={details.owner.cellPhone}
-					/>
-				)}
-			</div>
-			<div className={styles.containerRight}>
-				{!isLoading && (<h1>{details.pet.name}</h1>)}
+  return (
+    <div className={styles.mainContainerGrid}>
+      <div className={styles.containerLeft}>
+        {!isLoading && (
+          <CardUser
+            name={details.owner.name}
+            role={details.owner.role}
+            address={details.owner.address}
+            rating={details.owner.rating}
+            imgSrc={details.owner.profilePicture}
+            cellPhone={details.owner.cellPhone}
+          />
+        )}
+      </div>
+      <div className={styles.containerRight}>
+        {!isLoading && <h1>{details.pet.name}</h1>}
 
-				{!isLoading && <CardInfoPet
-					breed={details.pet.breed}
-					temperaments={details.pet.temperaments}
-					manners={details.pet.manners}
-					notes={details.pet.notes}
-				/> }
-			</div>
-		</div>
-	);
+        {!isLoading && (
+          <CardInfoPet
+            breed={details.pet.breed}
+            temperaments={details.pet.temperaments}
+            manners={details.pet.manners}
+            notes={details.pet.notes}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Details;
