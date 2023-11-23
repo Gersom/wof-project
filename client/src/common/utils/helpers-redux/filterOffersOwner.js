32fa1b598dbd432530a1fb86c7adb17c157256e9
EseToni <falsigfx@gmail.com>
2023-11-23 21:45:01 +0100

    [UPDATE] filters and sort logic

--- client/src/common/utils/helpers-redux/filterOffersOwner.js
+++ client/src/common/utils/helpers-redux/filterOffersOwner.js
@@ -1,18 +1,41 @@
+// export const filterOffersOwner = (offers, filters) => {
+// 	return Object.values(filters).reduce(
+// 		(filteredOffers, filter) => {
+// 			if (filter.name === '🐶 Perros' && filter.value) {
+// 				return filteredOffers.filter((offer) => offer.pet.species.name === 'dog');
+// 			} else if (filter.name === '🐯 Gatos' && filter.value) {
+                
+// 				return filteredOffers.filter((offer) => offer.pet.species.name === 'cat');
+// 			}  else {
+// 				return filteredOffers;
+// 			}
+// 		},
+// 		[...offers]
+// 	);
+// };
+
 export const filterOffersOwner = (offers, filters) => {
-	return Object.values(filters).reduce(
-		(filteredOffers, filter) => {
-			if (filter.name === '🐶 Perros' && filter.value) {
-				return filteredOffers.filter((offer) => offer.species === 'dog');
-			} else if (filter.name === '🐯 Gatos' && filter.value) {
-				return filteredOffers.filter((offer) => offer.species === 'cat');
-			} else if (filter.name === '♂️ Machos' && filter.value) {
-				return filteredOffers.filter((offer) => offer.gender === 'male');
-			} else if (filter.name === '♀️ Hembras' && filter.value) {
-				return filteredOffers.filter((offer) => offer.gender === 'female');
-			} else {
-				return filteredOffers;
-			}
-		},
-		[...offers]
-	);
-};
+    let filteredOffers = [...offers];
+  
+    for (const key in filters) {
+      filters[key].forEach((filter) => {
+        if (filter.name === '🐶 Perros' && filter.value) {
+          filteredOffers = filteredOffers.filter((offer) => offer.pet.species.name === 'dog');
+        } else if (filter.name === '🐯 Gatos' && filter.value) {
+          filteredOffers = filteredOffers.filter((offer) => offer.pet.species.name === 'cat');
+        } else if (filter.name === '♂️ Machos' && filter.value) {
+          filteredOffers = filteredOffers.filter((offer) => offer.gender === 'male');
+        } else if (filter.name === '♀️ Hembras' && filter.value) {
+          filteredOffers = filteredOffers.filter((offer) => offer.gender === 'female');
+        }
+      });
+    }
+  
+    return filteredOffers;
+  };
+
+// else if (filter.name === '♂️ Machos' && filter.value) {
+//     return filteredOffers.filter((offer) => offer.pet.gender === 'male');
+// } else if (filter.name === '♀️ Hembras' && filter.value) {
+//     return filteredOffers.filter((offer) => offer.pet.gender === 'female');
+// }
