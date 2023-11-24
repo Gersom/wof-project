c9558c570a412f50f841aa60719fb5147350bf20
EseToni <falsigfx@gmail.com>
2023-11-24 13:56:57 +0100

    [FIX] sorts rating

--- client/src/common/utils/helpers-redux/filterOffersOwner.js
+++ client/src/common/utils/helpers-redux/filterOffersOwner.js
@@ -1,18 +1,4 @@
-// export const filterOffersOwner = (offers, filters) => {
-// 	return Object.values(filters).reduce(
-// 		(filteredOffers, filter) => {
-// 			if (filter.name === '🐶 Perros' && filter.value) {
-// 				return filteredOffers.filter((offer) => offer.pet.species.name === 'dog');
-// 			} else if (filter.name === '🐯 Gatos' && filter.value) {
-                
-// 				return filteredOffers.filter((offer) => offer.pet.species.name === 'cat');
-// 			}  else {
-// 				return filteredOffers;
-// 			}
-// 		},
-// 		[...offers]
-// 	);
-// };
+
 
 export const filterOffersOwner = (offers, filters) => {
     let filteredOffers = [...offers];
@@ -33,9 +19,3 @@ export const filterOffersOwner = (offers, filters) => {
   
     return filteredOffers;
   };
-
-// else if (filter.name === '♂️ Machos' && filter.value) {
-//     return filteredOffers.filter((offer) => offer.pet.gender === 'male');
-// } else if (filter.name === '♀️ Hembras' && filter.value) {
-//     return filteredOffers.filter((offer) => offer.pet.gender === 'female');
-// }
