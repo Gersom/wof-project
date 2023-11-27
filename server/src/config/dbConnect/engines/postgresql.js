6db7d57fa963c702925ab8efdca56ba3d7cad1d3
Gersom-laptop <gersomalaja@gmail.com>
2023-11-27 13:41:51 -0500

    [UPDATE] config deploy

--- server/src/config/dbConnect/engines/postgresql.js
+++ server/src/config/dbConnect/engines/postgresql.js
@@ -1,15 +1,17 @@
 require('dotenv').config()
 const { Sequelize } = require("sequelize")
-// const { buildURI } = require("../../../utils/dbBuildURI")
+const { buildURI } = require("../../../utils/dbBuildURI")
 
-// const database = process.env.PG_DATABASE
-// const username = process.env.PG_USER
-// const password = process.env.PG_PASSWORD
-// const host = process.env.PG_HOST
+const database = process.env.PG_DATABASE
+const username = process.env.PG_USER
+const password = process.env.PG_PASSWORD
+const host = process.env.PG_HOST
 const uri = process.env.PG_URI
 
+const modeServer = process.env.MODE
+
 const sequelize = new Sequelize(
-  uri,
+  buildURI({ uri, database, username, password, host }),
   { logging: false } // Disable SQL log messages
 )
 
@@ -18,8 +20,10 @@ const dbConnectPostgresql = async () => {
     await sequelize.authenticate()
     console.log('*** PostgreSQL SUCCESS CONEXION ***');
 
-    await sequelize.sync({ force: true });
-    // await sequelize.sync();
+    let optionsSequelize = { force: true }
+    if (modeServer === 'prod') optionsSequelize = {}
+    
+    await sequelize.sync(optionsSequelize);
     console.log('- Models synchronization completed');
   } catch (e) {
     console.error('\n*** PostgreSQL ERROR CONEXION ***\n\n', e.message)
