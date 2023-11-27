8b375dd6c001d3d1cccc406cc5cefa9addc22f0e
giannafuselli <fuselligianna@gmail.com>
2023-11-27 17:32:00 -0300

    actualizacion landing

--- client/src/ui/pages/Landing/Landing.jsx
+++ client/src/ui/pages/Landing/Landing.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./styles.module.scss";
 import routerNames from "@src/common/constants/routes";
+import wofImage from "../../assets/images/landing/wof.svg";
 
 const LandingPage = () => {
   return (
@@ -11,13 +12,22 @@ const LandingPage = () => {
         </Link>
       </div>
       <div className={styles.container}>
-        <div className={styles.header}></div>
+        <div className={styles.header}>
+        <img src={wofImage} alt="Logo" className={styles.logo} />
+          <h1>Elige tu cuidador de mascotas perfecto</h1>
+        </div>
       </div>
       <div>
+        <p>O trabaja como cuidador profesional</p>
         <div className={styles.wrapper}></div>
         <div className={styles.btn}>
           <Link to={routerNames["register"]} className={styles.ctaButton}>
             Regístrate
+          </Link>
+        </div>
+        <div className={styles.btn}>
+          <Link to={routerNames["contact"]} className={styles.ctaButton}>
+            Contáctanos
           </Link>
         </div>
       </div>
@@ -26,3 +36,5 @@ const LandingPage = () => {
 };
 
 export default LandingPage;
+
+
