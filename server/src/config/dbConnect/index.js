47f1a91bd87188fcd167d886c8936d65a0224ab8
Gersom-laptop <gersomalaja@gmail.com>
2023-11-27 12:27:25 -0500

    [UPDATE] config DB CONNECT

--- server/src/config/dbConnect/index.js
+++ server/src/config/dbConnect/index.js
@@ -1,17 +1,17 @@
 require('dotenv').config()
 
-const DB_ENGINE = process.env.DB_ENGINE
-let dbConnect = ()=>null
+// const DB_ENGINE = process.env.DB_ENGINE
+let dbConnect = require("./engines/postgresql")
 
-switch (DB_ENGINE) {
-  case "postgresql":
-    dbConnect = require("./engines/postgresql")
-    break
-  case "mongodb":
-    dbConnect = require("./engines/mongodb")
-    break
-  default:
-    throw new Error("Environment variable 'DB_ENGINE' is not valid.")
-}
+// switch (DB_ENGINE) {
+//   case "postgresql":
+//     dbConnect = 
+//     break
+//   case "mongodb":
+//     dbConnect = require("./engines/mongodb")
+//     break
+//   default:
+//     throw new Error("Environment variable 'DB_ENGINE' is not valid.")
+// }
 
 module.exports = dbConnect
