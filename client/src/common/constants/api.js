86f5e3fc08c10ec55701d90ffdcd16a4d50c8851
Gersom-laptop <gersomalaja@gmail.com>
2023-11-27 14:53:34 -0500

    [UPDATE] url login and register

--- client/src/common/constants/api.js
+++ client/src/common/constants/api.js
@@ -2,11 +2,13 @@ const apiUrlEnv = import.meta.env.VITE_API_URL
 const apiUrlLocal = "http://localhost:3001"
 
 export const API = apiUrlEnv || apiUrlLocal;
-export const API_URL = API + '/api/';
+export const API_URL = API + '/api';
 
-export const API_URL_ITEMS = API_URL + "items/";
-export const API_URL_OFFERS = API_URL + "offers/";
-export const API_URL_PROVINCES = API_URL + "provinces/";
-export const API_URL_CREATE_USER = API_URL + "users/";
-export const API_URL_USER = API_URL + "users/";
-export const API_URL_REVIEWS = API_URL + "reviews/";
+export const API_URL_ITEMS = API_URL + "/items";
+export const API_URL_OFFERS = API_URL + "/offers";
+export const API_URL_PROVINCES = API_URL + "/provinces";
+export const API_URL_REVIEWS = API_URL + "/reviews";
+
+export const API_URL_USER = API_URL + "/users";
+export const API_URL_LOGIN = API_URL_USER + "/login";
+export const API_URL_REGISTER = API_URL_USER;
