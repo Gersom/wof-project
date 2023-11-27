484fa1efbb42fbeb73316f846c66abe7deae60e3
Gersom-laptop <gersomalaja@gmail.com>
2023-11-27 12:23:06 -0500

    [UPDATE] config postgresql

--- server/src/config/dbConnect/engines/postgresql.js
+++ server/src/config/dbConnect/engines/postgresql.js
@@ -1,15 +1,15 @@
 require('dotenv').config()
 const { Sequelize } = require("sequelize")
-const { buildURI } = require("../../../utils/dbBuildURI")
+// const { buildURI } = require("../../../utils/dbBuildURI")
 
-const database = process.env.PG_DATABASE
-const username = process.env.PG_USER
-const password = process.env.PG_PASSWORD
-const host = process.env.PG_HOST
+// const database = process.env.PG_DATABASE
+// const username = process.env.PG_USER
+// const password = process.env.PG_PASSWORD
+// const host = process.env.PG_HOST
 const uri = process.env.PG_URI
 
 const sequelize = new Sequelize(
-  buildURI({ uri, database, username, password, host }),
+  uri,
   { logging: false } // Disable SQL log messages
 )
 
