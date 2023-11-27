25b5e3c15eecbfc4364665340b88c288ae28fa7d
Gersom-laptop <gersomalaja@gmail.com>
2023-11-27 16:18:07 -0500

    [UPDATE] listen server to deploy

--- server/src/config/listen.js
+++ server/src/config/listen.js
@@ -1,8 +1,17 @@
-const generateServerPath = require("./../utils/generateServerPath")
+require('dotenv').config()
 
-const { port, path } = generateServerPath()
+const modeEnv = process.env.MODE
+let path = ''
+if (modeEnv) {
+  const port = process.env.PORT || 3000
+  const host = process.env.HOST || 'http://localhost'
+  path = `${host}:${port}`
+} else path = host
+
 const message = 'Your server is ready'
 const allMessage = `\n${message}:\n=> ${path}\n`
+
+
 
 // LISTEN
 const listen = (app, cb= ()=>null) => {
