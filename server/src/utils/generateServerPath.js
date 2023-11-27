814891bea2e690fc472cc38eeea087749a1a9746
Gersom-laptop <gersomalaja@gmail.com>
2023-11-27 15:52:37 -0500

    [UPDATE] genertateServePath for deploy

--- server/src/utils/generateServerPath.js
+++ server/src/utils/generateServerPath.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 
 const generateServerPath = () => {
+  const modeEnv = process.env.MODE
   const port = process.env.PORT || 3000
   const host = process.env.HOST || 'http://localhost'
-  const path = `${host}:${port}`
+  const path = modeEnv ? host : `${host}:${port}`
 
   return { port, host, path }
 }
