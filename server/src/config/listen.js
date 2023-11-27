b0a574fbfe1af73f6b3096a15a703d03872b5fed
Gersom-laptop <gersomalaja@gmail.com>
2023-11-27 16:28:34 -0500

    [UPDATE] config listen

--- server/src/config/listen.js
+++ server/src/config/listen.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
 
-const modeEnv = process.env.MODE
 let path = ''
-if (modeEnv) {
-  const port = process.env.PORT || 3000
-  const host = process.env.HOST || 'http://localhost'
-  path = `${host}:${port}`
-} else path = host
+const modeEnv = process.env.MODE
+const port = process.env.PORT || 3000
+const host = process.env.HOST || 'http://localhost'
+
+if (modeEnv) path = `${host}:${port}`
+else path = host
 
 const message = 'Your server is ready'
 const allMessage = `\n${message}:\n=> ${path}\n`
