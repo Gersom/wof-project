8dbbef5b4892eb5727eae4233977e77a0707e8fc
Gersom-laptop <gersomalaja@gmail.com>
2023-11-27 14:09:48 -0500

    [UPDATE] constants api add env

--- client/src/common/constants/api.js
+++ client/src/common/constants/api.js
@@ -1,4 +1,8 @@
-export const API_URL = "http://localhost:3001/api/";
+const apiUrlEnv = import.meta.env.VITE_API_URL
+const apiUrlLocal = "http://localhost:3001/api/"
+
+export const API_URL = apiUrlEnv || apiUrlLocal;
+
 export const API_URL_ITEMS = API_URL + "items/";
 export const API_URL_OFFERS = API_URL + "offers/";
 export const API_URL_PROVINCES = API_URL + "provinces/";
