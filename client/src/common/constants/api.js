0b7f78ec3e56ebac23eea428052aef67dd48a6b3
alejoo61 <alejtk61@gmail.com>
2023-11-27 21:17:05 -0300

    [ADD] mailing config/ controllers/ env/

--- client/src/common/constants/api.js
+++ client/src/common/constants/api.js
@@ -3,9 +3,5 @@ export const API_URL_ITEMS = API_URL + "items/";
 export const API_URL_OFFERS = API_URL + "offers/";
 export const API_URL_PROVINCES = API_URL + "provinces/";
 export const API_URL_CREATE_USER = API_URL + "users/";
-<<<<<<< HEAD
-export const APU_URL_LOGIN = API_URL + "login/";
-=======
+export const API_URL_LOGIN = API_URL + "login/";
 export const API_URL_USER = API_URL + "users/";
-export const API_URL_REVIEWS = API_URL + "reviews/";
->>>>>>> 92a9965ff79ff57ff2aa61f4b926137e5a0488cc
--- client/src/ui/components/forms/formRegister/validation.js
+++ client/src/ui/components/forms/formRegister/validation.js
@@ -1,38 +1,40 @@
 export const validation = (dataForm, errors, setErrors) => {
-  // Validar el Nombre
-  if (dataForm.name.length > 30) {
+  // Validar el Correo Electrónico
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(dataForm.email)) {
     setErrors({
       ...errors,
-      name: "El Nombre no puede superar los 30 caracteres",
+      email: "Ingrese un correo electrónico válido",
     });
+  } else if (dataForm.email.trim() === "") {
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      email: "El campo está vacío",
+    }));
+  } else {
+    setErrors((prevErrors) => ({ ...prevErrors, email: "" }));
+  }
+
+  // Validar el Nombre
+  if (dataForm.name.length < 3) {
+    setErrors({
+      ...errors,
+      name: "El Nombre debe conterner al menos 3 caracteres",
+    });
+  } else if (dataForm.name.length > 30) {
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      name: "El Nombre no puede superar los 30 caracteres",
+    }));
   } else if (
     /\d/.test(dataForm.name) ||
     /[!@#$%^&*(),.?":{}|<>]/.test(dataForm.name)
   ) {
-    setErrors({
-      ...errors,
+    setErrors((prevErrors) => ({
+      ...prevErrors,
       name: "El nombre no debe contener números o caracteres especiales",
-    });
+    }));
   } else {
     setErrors((prevErrors) => ({ ...prevErrors, name: "" }));
-  }
-
-  // Validar el Apellido
-  if (dataForm.lastname.length > 30) {
-    setErrors({
-      ...errors,
-      lastname: "El Apellido no puede superar los 30 caracteres",
-    });
-  } else if (
-    /\d/.test(dataForm.lastname) ||
-    /[!@#$%^&*(),.?":{}|<>]/.test(dataForm.lastname)
-  ) {
-    setErrors({
-      ...errors,
-      lastname: "El Apellido no debe contener números o caracteres especiales",
-    });
-  } else {
-    setErrors((prevErrors) => ({ ...prevErrors, lastname: "" }));
   }
 
   // Validar la Contraseña
@@ -61,18 +63,26 @@ export const validation = (dataForm, errors, setErrors) => {
     setErrors((prevErrors) => ({ ...prevErrors, password: "" }));
   }
 
-  // Validar el Correo Electrónico
-  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(dataForm.email)) {
+  // Validar el Apellido
+  if (dataForm.lastName.length < 3) {
     setErrors({
       ...errors,
-      email: "Ingrese un correo electrónico válido",
+      lastName: "El Nombre debe conterner al menos 3 caracteres",
     });
-  } else if (dataForm.email.trim() === "") {
+  } else if (dataForm.lastName.length > 30) {
     setErrors({
       ...errors,
-      email: "El campo está vacío",
+      lastName: "El Apellido no puede superar los 30 caracteres",
+    });
+  } else if (
+    /\d/.test(dataForm.lastName) ||
+    /[!@#$%^&*(),.?":{}|<>]/.test(dataForm.lastName)
+  ) {
+    setErrors({
+      ...errors,
+      lastName: "El Apellido no debe contener números o caracteres especiales",
     });
   } else {
-    setErrors((prevErrors) => ({ ...prevErrors, email: "" }));
+    setErrors((prevErrors) => ({ ...prevErrors, lastName: "" }));
   }
 };
