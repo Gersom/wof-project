340ec13363e017ab69cfa795d9bcfb23a98d6b2c
JBartolo <jesusbartolojbh@gmail.com>
2023-11-29 14:47:30 -0500

    [ADD] new route 'breeds'

--- server/src/routes/countries.js
+++ server/src/routes/countries.js
@@ -7,18 +7,3 @@ const controllers = require("../controllers/countries")
 router.get("/", controllers.getCountries)
 
 module.exports = router
-
-// const {CountriesModel} = require("./../models/index")
-
-// const express = require("express");
-// const router = express.Router();
-
-// router.get("/", async (req, res) => {
-//   const countries = await CountriesModel.findAllData()
-//   if (countries) {
-//     res.status(200).json(countries)
-//   }
-//   else res.status(200).json({error: "Countries not found"})
- 
-// });
-// module.exports = router
