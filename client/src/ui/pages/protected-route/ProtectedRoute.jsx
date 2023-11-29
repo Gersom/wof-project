8dc85f5a66b791019fd2311eff9c0d0d55d7c7f9
DaroOps <romerosd089@hotmail.com>
2023-11-29 13:54:17 -0500

    [ADD] component for protecting routes

---
+++ client/src/ui/pages/protected-route/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import { Outlet, Navigate } from "react-router-dom";
+import routerNames from "@src/common/constants/routes";
+import { useAuth } from "@src/context/auth-provider/authProvider";
+
+export default function ProtectedRoute(){
+    const auth = useAuth();
+
+    return auth.isAuthenticated? <Outlet/>:<Navigate to={routerNames["login"]}/>
+}
