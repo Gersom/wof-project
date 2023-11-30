cb78fa72634a58032b097e7a23d15c01f5f00f6a
Abril31 <brivaldi0416@gmail.com>
2023-11-30 11:36:56 -0400

    [UPDATE] CareInProgress

--- client/src/ui/pages/Landing/Landing.jsx
+++ client/src/ui/pages/Landing/Landing.jsx
@@ -6,28 +6,25 @@ import rectangle from "@images/landing/rectangle.svg";
 
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect } from "react";
+import CareInProgress from "@src/ui/components/care-in-progress/CareInProgress";
 
 const LandingPage = () => {
-  
-  const {isLoading, isAuthenticated:isAuth0enticated} = useAuth0();
+  const { isLoading, isAuthenticated: isAuth0enticated } = useAuth0();
 
-
-  useEffect(()=>{
-    if(isLoading){
+  useEffect(() => {
+    if (isLoading) {
       console.log("LOADING AUTH0 INFO", isLoading);
-    }
-    else if(!isLoading && isAuth0enticated){
+    } else if (!isLoading && isAuth0enticated) {
+      console.log("LOGIN FINISH VERIFICATION AUTH0 RETURNS", isAuth0enticated);
+    } else if (!isLoading && !isAuth0enticated) {
       console.log("LOGIN FINISH VERIFICATION AUTH0 RETURNS", isAuth0enticated);
     }
-    else if(!isLoading && !isAuth0enticated){
-      console.log("LOGIN FINISH VERIFICATION AUTH0 RETURNS", isAuth0enticated);
-    }
-  },[isLoading, isAuth0enticated])
-
+  }, [isLoading, isAuth0enticated]);
 
   return (
     <div className={styles.mainContainer}>
       <div>
+        <CareInProgress />
         <img className={styles.imageRec} src={rectangle} alt="Rectangle" />
       </div>
       <div className={styles.cont}>
@@ -36,7 +33,7 @@ const LandingPage = () => {
         </Link>
         <Link to={routerNames["login"]} className={styles.loginBtn}>
           Inicia Sesión
-        </Link> 
+        </Link>
       </div>
       <div className={styles.container}>
         <h1 className={styles.title}>Elige tu cuidador de mascotas perfecto</h1>
