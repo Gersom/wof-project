0b830380640232bea3051319e7f87277063d9ead
DaroOps <romerosd089@hotmail.com>
2023-12-01 09:56:47 -0500

    [HOTFIX] caregiver post offer button

--- client/src/ui/components/cards/card-accept/cardAccept.jsx
+++ client/src/ui/components/cards/card-accept/cardAccept.jsx
@@ -1,19 +1,19 @@
 import styles from './styles.module.scss';
 
-const CardAccept = ({ 
+const CardAccept = ({
   startDate,
   endDate,
   completedAcept,
-  onAccept= ()=>null,
+  onAccept = () => null,
   toggleModal
 }) => {
 
   const formatDate = (fechaString) => {
     if (fechaString) {
       const fecha = new Date(fechaString);
-  
-      const opcionesDeFormato = { year: 'numeric', month: 'long', day: 'numeric'};
-  
+
+      const opcionesDeFormato = { year: 'numeric', month: 'long', day: 'numeric' };
+
       const formato = new Intl.DateTimeFormat('es-ES', opcionesDeFormato);
       const fechaFormateada = formato.format(fecha);
       return fechaFormateada
@@ -26,25 +26,29 @@ const CardAccept = ({
   }
   return (
     <article className={styles.article}>
-        <h3>Publicación</h3>
-        <div className={styles.info}>
-            <h4>Tiempo a cuidar:</h4>
-        </div>
-        <div className={styles.info}>
-            <h5>Desde: {formatDate(startDate)}</h5>
-        </div>
-        <div className={styles.info}>
-            <h5>Hasta: {formatDate(endDate)}</h5>
-        </div>
-        {
-          ! completedAcept ?
+      <h3>Publicación</h3>
+      <div className={styles.info}>
+        <h4>Tiempo a cuidar:</h4>
+      </div>
+      <div className={styles.info}>
+        <h5>Desde: {formatDate(startDate)}</h5>
+      </div>
+      <div className={styles.info}>
+        <h5>Hasta: {formatDate(endDate)}</h5>
+      </div>
+      {
+        !completedAcept ?
           <button className={styles.linkContainer}
-          onClick={handleAccept}>
+            onClick={handleAccept}>
             Deseo Cuidar
           </button>
-          : ''
-        }
-        
+          :
+          <button className={styles.cancelButton}
+            onClick={handleAccept}>
+            Cancelar oferta
+          </button>
+      }
+
     </article>
   )
 }
