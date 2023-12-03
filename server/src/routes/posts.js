ac60f3261a46fc70e6d2b5604e8728ae831d467a
Gersom-laptop <gersomalaja@gmail.com>
2023-12-03 15:39:01 -0500

    [UPDATE] posts caregiverid query

--- server/src/routes/posts.js
+++ server/src/routes/posts.js
@@ -4,11 +4,32 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const countries = await PostsModel.findAllData()
-  if (countries) {
-    res.status(200).json(countries)
+  const caregiverId = req.query.caregiverId
+  if (caregiverId) {
+    const posts = await PostsModel.findByCaregiverId(caregiverId)
+    if (posts) {
+      let postsFormated = posts
+      postsFormated = postsFormated.map((post) => {
+        post = post.toJSON()
+        return {
+          ...post,
+          pet:{
+            id: post?.pet?.id,
+            name: post?.pet?.name,
+            imageUrl: post?.pet?.petsImages[0]?.imageUrl
+          }
+        }
+      })
+      res.status(200).json(postsFormated)
+    }
+    else res.status(403).json({error: "Posts not found"})
+  } else {
+    const countries = await PostsModel.findAllData()
+    if (countries) {
+      res.status(200).json(countries)
+    }
+    else res.status(200).json({error: "Posts not found"})
   }
-  else res.status(200).json({error: "Posts not found"})
 });
 
 
