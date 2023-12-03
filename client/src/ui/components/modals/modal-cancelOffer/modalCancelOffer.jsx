ce01e9309a8981d975010544c5d799ded833f00e
DaroOps <romerosd089@hotmail.com>
2023-12-03 13:26:20 -0500

    [UPDATE] delete offer and rebuild modals (cancel offer,send offer)

--- client/src/ui/components/modals/modal-cancelOffer/modalCancelOffer.jsx
+++ client/src/ui/components/modals/modal-cancelOffer/modalCancelOffer.jsx
@@ -1,14 +1,33 @@
-import styles from "./styles.module.scss";
+import { API_URL } from "@src/common/constants/api";
 
-const ModalCancelOffer = ({ message, nameOwner }) => {
+import styles from "./styles.module.scss";
+import axios from "axios";
+
+const ModalCancelOffer = ({ message, nameOwner , data}) => {
+
+  const cancelOffer = async (data) => {
+    try {
+      const response = await axios.delete(API_URL + "/requests/"+`${data.id}`);
+      //console.log(response);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  //console.log(data);
+  const asyncOperation = async() =>{
+    await cancelOffer(data);
+  }
+  asyncOperation();
+
   return (
     <div className={styles.container}>
       <h1>¡OH NO!</h1>
-      <h3>Oferta cancelada</h3>
+      <h3>OFERTA CANCELADA</h3>
       <section>
-        <h4>
-          Tu oferta ya ha sido eliminada. ¡Esperemos verte en otra oferta!
-        </h4>
+        <h4>Tu oferta la hemos eliminado</h4>
+        <h2>{nameOwner}</h2>
+        <h4> ya no podra ver tu oferta</h4>
       </section>
     </div>
   );
