b87aff0d6b4318a39a9729506f2426e938bd82ef
Gersom-laptop <gersomalaja@gmail.com>
2023-12-02 23:36:18 -0500

    [UPDATE] button default

--- client/src/ui/components/buttons/DefaultButton.jsx
+++ client/src/ui/components/buttons/DefaultButton.jsx
@@ -1,30 +1,26 @@
 import styles from './styles.module.scss';
 
 const DefaultButton = ({ 
-  onAction= ()=>null, 
-  align = 'center', 
+  onAction= ()=>null,
   label = "Aceptar",
   iconFill = "fill",
+  disabled = false,
   children,
 }) => {
 
-  const content = (<>
-    {children}
-
-    <span className={styles.defaultButtonText}>
-      {label}
-    </span>
-  </>)
-
   const generateClasses = () => {
     let classes = `${styles.defaultButton}`
-
-    if (align === 'left') {
-      classes += ` ${styles.defaultButtonLeft}`
-    }
     
-    if (iconFill === 'fill') {
+    if (!disabled) {
+      classes += ` ${styles.defaultButtonEnabled}`
+    } else {
+      classes += ` ${styles.defaultButtonDisabled}`
+    }
+
+    if (iconFill === 'fill' && !disabled) {
       classes += ` ${styles.defaultButtonIconFill}`
+    } else if (iconFill === 'fill' && disabled) {
+      classes += ` ${styles.defaultButtonIconFillDisabled}`
     }
 
     return classes
@@ -32,9 +28,13 @@ const DefaultButton = ({
 
   return (<>
     <button 
+      disabled={disabled}
       className={generateClasses()} 
       onClick={onAction}>
-      {content}
+      {children}
+      <span className={styles.defaultButtonText}>
+        {label}
+      </span>
     </button>
   </>)
 };
