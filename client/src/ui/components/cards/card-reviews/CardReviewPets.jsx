f3f3e054d6335fa71004a92c650901210df857ff
DaroOps <romerosd089@hotmail.com>
2023-12-03 15:25:19 -0500

    [UPDATE] remove harcode of reviews in caregivers/offers/details/*

--- client/src/ui/components/cards/card-reviews/CardReviewPets.jsx
+++ client/src/ui/components/cards/card-reviews/CardReviewPets.jsx
@@ -1,53 +1,16 @@
 import { useState, useEffect } from "react";
 import styles from "./styles.module.scss";
-// import { getReviews } from "@src/common/utils/helpers-redux/getReviews";
-// import { useDispatch, useSelector } from "react-redux";
 
-const CardReviewPets = () => {
-  let reviews = [
-    {
-      id: 1,
-      rating: "4.5",
-      comment:
-        "Anny fue muy correcta al describir su mascota, me gusto tenerlo en mi casa, no causa problemas y avisa cuando tiene que ir al baño.",
-      from: "caregiver",
-      to: "owner",
-      createdAt: "2023-11-25T17:49:07.213Z",
-      updatedAt: "2023-11-25T17:49:07.213Z",
-      caregiverId: 1,
-      ownerId: 1,
-      name: "Gabriel",
-      image:
-        "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    },
-    {
-      id: 2,
-      rating: "2.0",
-      comment:
-        "Ese perro es muy problemático, me saca la lengua a cada rato, me babea el mueble, cero recomendado.",
-      from: "caregiver",
-      to: "owner",
-      createdAt: "2023-11-25T17:49:07.213Z",
-      updatedAt: "2023-11-25T17:49:07.213Z",
-      caregiverId: 2,
-      ownerId: 1,
-      name: "Gaby",
-      image:
-        "https://images.pexels.com/photos/1310522/pexels-photo-1310522.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    },
-  ];
-
-  // const dispatch = useDispatch();
-  // const reviews = useSelector((reviewsState) => reviewsState.reviews);
-
-  // useEffect(() => {
-  //   dispatch(getReviews());
-  // }, []);
+const CardReviewPets = ({reviewsData}) => {
+  
   const [formattedDates, setFormattedDates] = useState([]);
 
   const formatDates = () => {
-    const formatted = reviews.map((review) => {
-      const reviewDate = new Date(review.createdAt);
+    if (!reviewsData) {
+      return; 
+    }
+    const formatted = reviewsData?.map((review) => {
+      const reviewDate = new Date(review.date);
       const options = { year: "numeric", month: "long" };
 
       const monthName = reviewDate
@@ -73,22 +36,22 @@ const CardReviewPets = () => {
       <div className={styles.cardCont}>
         <h3 className={styles.title}>Reseñas</h3>
         <hr className={styles.firstRounded}></hr>
-        {reviews &&
-          reviews.map((review, index) => (
+        {reviewsData &&
+          reviewsData.map((review, index) => (
             <div key={review.id}>
               <h5 className={styles.paragraph}>"{review.comment}"</h5>
               <div className={styles.infoPerson}>
                 <img
                   className={styles.profilePic}
-                  src={review.image}
+                  src={review.caregiver.profilePicture}
                   alt="Foto de Perfil"
                 />
                 <div className={styles.textInfo}>
-                  <p className={styles.personName}>{review.name}</p>
+                  <p className={styles.personName}>{review.caregiver.name}</p>
                   <p className={styles.date}>{formattedDates[index]}</p>
                 </div>
               </div>
-              {index < reviews.length - 1 && (
+              {index < reviewsData.length - 1 && (
                 <hr className={styles.secondRounded}></hr>
               )}
             </div>
