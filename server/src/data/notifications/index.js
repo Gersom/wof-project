102cf97196b796409644c60cf0d6bb4802f7550a
Chrislorusso07 <chrislorutty@gmail.com>
2023-12-05 18:24:20 -0300

    UPDATE [NOTIFICACION CREADA] REVIEW CREADA Y REVIEW MODIFICADA

--- server/src/data/notifications/index.js
+++ server/src/data/notifications/index.js
@@ -1,26 +1,44 @@
-const changePassword = 
-{
-        "message": "password changed",
-        "status": false,
-        "action": ""
-}
+const changePassword = {
+  message: "password changed",
+  status: false,
+  action: "",
+};
 
-const createdUser = 
-{
-        "message": "Se crearon correctamente tus claves de ingreso, bienvenido",
-        "status": false,
-        "action": ""
-}
+const createdUser = {
+  message: "Se crearon correctamente tus claves de ingreso, bienvenido",
+  status: false,
+  action: "",
+};
 
-const createdTransaction = 
-{
-        "message": "Tu pago se registro correctamente",
-        "status": false,
-        "action": ""
-}
+const createdTransaction = {
+  message: "Tu pago se registro correctamente",
+  status: false,
+  action: "",
+};
 
-module.exports = { 
+const createdPet = {
+  message: "Tu mascota se creó correctamente",
+  status: false,
+  action: "",
+};
+
+const createdReview = {
+  message: "Tu review se ha publicado correctamente",
+  status: false,
+  action: "",
+};
+
+const updatedReview = {
+  message: "Tu review se ha actualizado correctamente",
+  status: false,
+  action: "",
+};
+
+module.exports = {
   changePassword,
   createdUser,
-  createdTransaction
-}
+  createdTransaction,
+  createdPet,
+  createdReview,
+  updatedReview,
+};
--- server/src/logic/reviews.js
+++ server/src/logic/reviews.js
@@ -1,52 +1,63 @@
-const { ReviewsModel, UsersModel } = require("../models");
+const { ReviewsModel, UsersModel, NotificationsModel } = require("../models");
+const { createdReview, updatedReview } = require("../data/notifications");
 
 const getAllReviewsLogic = async (ownerid) => {
-  const reviews = await ReviewsModel.findAllData()
-  if(ownerid){
-    return await ReviewsModel.findByOwner(ownerid)
+  const reviews = await ReviewsModel.findAllData();
+  if (ownerid) {
+    return await ReviewsModel.findByOwner(ownerid);
   }
-  return reviews
-}
+  return reviews;
+};
 
 const getReviewLogic = async (id) => {
-  const review = await ReviewsModel.findDataById(id)
-  if (!review) throw Error("User not found")
-  return review
+  const review = await ReviewsModel.findDataById(id);
+  if (!review) throw Error("User not found");
+  return review;
 };
 
 const postReviewLogic = async (data) => {
-  const {rating, from, to, caregiverId, ownerId } = data
-  if(!rating || !from || !to || !caregiverId || !ownerId ) throw Error('./missing data')
-  if(from === to) throw Error(`'from' and 'to' cannot be equal`)
+  const { rating, from, to, caregiverId, ownerId } = data;
+  if (!rating || !from || !to || !caregiverId || !ownerId)
+    throw Error("./missing data");
+  if (from === to) throw Error(`'from' and 'to' cannot be equal`);
 
-  const newReview = await ReviewsModel.createReview(data)
+  const newReview = await ReviewsModel.createReview(data);
+  await NotificationsModel.create({
+    ...createdReview,
+    ownerId: data.ownerId,
+    caregiverId: data.caregiverId,
+  });
 
-  return newReview
-  }
+  return newReview;
+};
 //   return {
 //     success: 'The user was created successfully.'
 //   }
 
 const updateReviewLogic = async (id, data) => {
-  const { comment, rating } = data
- 
-  await ReviewsModel.updateData(id, {comment,rating})
+  const { comment, rating } = data;
+
+  await ReviewsModel.updateData(id, { comment, rating });
+  await NotificationsModel.create({
+    ...updatedReview,
+  });
+  console.log(pruebaNoti);
 
   return {
-    success: 'User was update correctly.'
-  }
-}
+    success: "User was update correctly.",
+  };
+};
 const deleteReviewLogic = async (id, data) => {
-  await ReviewsModel.removeData(id)
+  await ReviewsModel.removeData(id);
   return {
-    success: 'User was deleted correctly.'
-  }
-}
+    success: "User was deleted correctly.",
+  };
+};
 
 module.exports = {
   getAllReviewsLogic,
   getReviewLogic,
   postReviewLogic,
   updateReviewLogic,
-  deleteReviewLogic
+  deleteReviewLogic,
 };
