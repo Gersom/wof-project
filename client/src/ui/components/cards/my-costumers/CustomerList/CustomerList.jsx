d28ff72d4ec84c6beff6ddd3ae150e5bee89af4a
Abril31 <brivaldi0416@gmail.com>
2023-12-05 16:57:49 -0400

    [UPDATE] get info from databae in MyClients

--- client/src/ui/components/cards/my-costumers/CustomerList/CustomerList.jsx
+++ client/src/ui/components/cards/my-costumers/CustomerList/CustomerList.jsx
@@ -1,10 +1,16 @@
 import styles from "./styles.module.scss";
 import CustomerCardModel from "../CustomerCard/CostumerCardModel";
 const CustomerList = ({ customers }) => {
+  const generateUniqueKey = (customer) => {
+    return `${customer.owner.name}_${customer.pet.name}_${customer.addrress}`;
+  };
   return (
     <article className={styles.article}>
       {customers.map((customer) => (
-        <CustomerCardModel key={customer.id} customerData={customer} />
+        <CustomerCardModel
+          key={generateUniqueKey(customer)}
+          customerData={customer}
+        />
       ))}
     </article>
   );
