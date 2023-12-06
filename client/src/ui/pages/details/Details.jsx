997e8e3c4cdaa7a9558517d3287efce82bccfa8f
EseToni <falsigfx@gmail.com>
2023-12-06 16:46:31 +0100

    [UPDATE] detailsOffer el boton de aceptar y cancelar oferta ya funciona correctamente

--- client/src/ui/pages/details/Details.jsx
+++ client/src/ui/pages/details/Details.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./styles.module.scss";
 import CardUser from "@src/ui/components/cards/card-user/CardUser";
 import Carousel from "@src/ui/components/carousel/Carousel";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 import useGetDetails from "@src/common/hooks/useGetDetails";
 import CardInfoPet from "@src/ui/components/cards/card-info/CardInfoPet";
 import CardReviewPets from "@src/ui/components/cards/card-reviews/CardReviewPets";
@@ -11,6 +12,7 @@ import ModalPrice from "@src/ui/components/modals/modal-price/ModalPrice";
 import ModalCustom from "@src/ui/components/modals/modal-custom/ModalCustom";
 import ModalSendOffer from "@src/ui/components/modals/modal-send-offer/SendOffer";
 import ModalCancelOffer from "@src/ui/components/modals/modal-cancel-offer/ModalCancelOffer";
+import { API_URL_SERVICES } from "@src/common/constants/api";
 
 const Details = () => {
   const { id } = useParams();
@@ -19,9 +21,21 @@ const Details = () => {
   const [statusModal, setStatusModal] = useState(false);
   const [payedInfoModal, setPayedInfoModal] = useState(false);
   const [cancelModal, setCancelModal] = useState(false);
-
+  const caregiverId = useSelector((state) => state?.userReducer?.user?.caregiver?.id);
   // console.log(details);
 
+  useEffect(() => {
+    const getRequest = async () => {
+      const response = await fetch(`${API_URL_SERVICES}${id}`);
+      const data = await response.json();
+      if (data.some((item) => item.caregiverId === caregiverId)) {
+        setSuccess(true);
+      }else{
+        setSuccess(false);
+      }
+    }
+    getRequest();
+  },[id, caregiverId])
   
   const manageModal = () => {
     setStatusModal(false)
@@ -34,11 +48,24 @@ const Details = () => {
     setStatusModal(true);
   };
 
-  const cancelFunc = () => {
+  const cancelFunc = async () => {
     setCancelModal(false);
     setStatusModal(false);
+    let options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({caregiverId: caregiverId}),
+    };
+    const response = await fetch(`${API_URL_SERVICES}${id}`, options);
+    const data = await response.json();
+    if (data) {
+      setSuccess(false);
+    }
   };
-  // console.log("DETAILS DETAILS DETAILS",details);
+
+
   return (
     <div className={styles.mainContainerGrid}>
       <div className={styles.containerLeft}>
@@ -79,12 +106,13 @@ const Details = () => {
               <ModalPrice
                 data={details}
                 toggleModal={() => manageModal()}
+                setSuccess={() => setSuccess(true)}
               />
 
             </ModalCustom>
             <ModalCustom
               state={payedInfoModal}
-              toggleModal={() => { setPayedInfoModal(false); setSuccess(true); }}
+              toggleModal={() => { setPayedInfoModal(false) }}
             >
               <ModalSendOffer nameOwner={details.owner.name}/>
             </ModalCustom>
