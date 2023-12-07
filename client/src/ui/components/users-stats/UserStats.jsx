12a0adb36077d50f7e1793db7af2412fb013870d
Chrislorusso07 <chrislorutty@gmail.com>
2023-12-07 17:59:39 -0300

    USERS UPDATE

---
+++ client/src/ui/components/users-stats/UserStats.jsx
@@ -0,0 +1,12 @@
+const UserStats = () => {
+  return (
+    <div>
+      <section>🧑‍🎄</section>
+      <section>🐕</section>
+      <section>🐶</section>
+      <section>🤝</section>
+    </div>
+  );
+};
+
+export default UserStats;
--- client/src/ui/pages/users/Users.jsx
+++ client/src/ui/pages/users/Users.jsx
@@ -1,9 +1,11 @@
+import UserStats from "@src/ui/components/users-stats/UserStats";
 import styles from "./styles.module.scss";
 
 const Users = () => {
   return (
     <div className={styles.containerUsers}>
       <h1 className={styles.usuarios}>Usuarios</h1>
+      <UserStats></UserStats>
     </div>
   );
 };
