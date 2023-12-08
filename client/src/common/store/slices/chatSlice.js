25a0923d3113a40f915898e11d813e6d5e01d708
EseToni <falsigfx@gmail.com>
2023-12-09 00:23:59 +0100

    [UPDATE] Chat ahora recibe notificaciones via redux

--- client/src/common/store/slices/chatSlice.js
+++ client/src/common/store/slices/chatSlice.js
@@ -32,12 +32,15 @@ const chatSlice = createSlice({
 			state.msgtotal = state.msgtotal += 1;
 		},
 		cleanChat: (state, action) => {
+      let numberNot = 0
 			state.chats = [...state.chats].map((chat) => {
 				if (chat.id === action.payload.id) {
+          numberNot = chat.notifications
 					chat.notifications = 0;
 				}
 				return chat;
 			});
+      state.msgtotal = state.msgtotal - numberNot
 		},
 	},
 });
