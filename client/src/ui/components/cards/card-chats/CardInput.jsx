20363168e4f0388eb10cacc71cd338048f30292c
EseToni <falsigfx@gmail.com>
2023-12-09 22:11:27 +0100

    [ADD] chat carga imagen desde el local

--- client/src/ui/components/cards/card-chats/CardInput.jsx
+++ client/src/ui/components/cards/card-chats/CardInput.jsx
@@ -1,21 +1,37 @@
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
 
+const CardInput = ({ value, setValue, sendMessage, setImageFile, setImageLocal }) => {
+	const handleKeyPress = (e) => {
+		if (e.key === 'Enter' && !e.shiftKey) {
+			e.preventDefault();
+			sendMessage();
+		}
+	};
 
- const CardInput = ({value , setValue , sendMessage}) => {
+  const handleSetImage = async (event) => {
+    const file = event.target.files[0];
+		if (file) {
+			file.preview = URL.createObjectURL(file);
+			setImageFile(file)
+			setImageLocal(file.preview);
+      console.log(file)
+		}
+  }
 
-  const handleKeyPress = (e) => {
-    if (e.key === 'Enter' && !e.shiftKey) {
-      e.preventDefault(); 
-      sendMessage();
-    }
-  };
+	return (
+		<div className={styles.containerInput}>
+			<textarea
+				value={value}
+				onChange={(e) => setValue(e.target.value)}
+				onKeyPress={handleKeyPress}
+				placeholder='Escribe un mensaje!'
+			/>
+			<div className={styles.containerBtnInput}>
+				<input className={styles.btnImg} type='file' onChange={handleSetImage}/>
+				<button onClick={sendMessage}>Enviar</button>
+			</div>
+		</div>
+	);
+};
 
-  return (
-    <div className={styles.containerInput}>
-      <textarea value={value} onChange={(e)=> setValue(e.target.value)} onKeyPress={handleKeyPress} placeholder='Escribe un mensaje!'/>
-      <button onClick={sendMessage}>Enviar</button>
-    </div>
-  )
-}
-
-export default CardInput
+export default CardInput;
