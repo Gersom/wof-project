f9b91d875d542b919f03bcc34e0e4b47181965ea
EseToni <falsigfx@gmail.com>
2023-12-12 15:39:26 +0100

    [UPDATE] ws ahora espera para reconectar

--- client/src/common/utils/websocket/useWsCaregiver.jsx
+++ client/src/common/utils/websocket/useWsCaregiver.jsx
@@ -1,99 +1,117 @@
-import { WS_URL } from '@src/common/constants/api';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { actionGetOffersOwner } from '@src/common/store/actions/offersActions';
-import { setAlert } from '@src/common/store/slices/alertSlice';
-import { setWs } from '@src/common/store/slices/wsSlice';
-import routerNames from '@src/common/constants/routes';
-import { useLocation } from 'react-router-dom';
+import { WS_URL } from "@src/common/constants/api";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { actionGetOffersOwner } from "@src/common/store/actions/offersActions";
+import { setAlert } from "@src/common/store/slices/alertSlice";
+import { setWs } from "@src/common/store/slices/wsSlice";
+import routerNames from "@src/common/constants/routes";
+import { useLocation } from "react-router-dom";
 
 const useWsCaregiver = () => {
-	const dispatch = useDispatch();
-	const location = useLocation();
-	
-	const [lastProcessedMessage, setLastProcessedMessage] = useState(null);
+  const dispatch = useDispatch();
+  const location = useLocation();
 
-	const caregiverId = useSelector((state) => state.userReducer.user?.caregiver?.id);
-	const wsCaregiver = useSelector((state) => state.wsReducer.ws);
-	const ROLE = useSelector((state) => state?.userReducer?.user?.role);
+  const [lastProcessedMessage, setLastProcessedMessage] = useState(null);
 
-	useEffect(() => {
+  const caregiverId = useSelector(
+    (state) => state.userReducer.user?.caregiver?.id
+  );
+  const wsCaregiver = useSelector((state) => state.wsReducer.ws);
+  const ROLE = useSelector((state) => state?.userReducer?.user?.role);
 
-		if (ROLE !== 'caregiver') return;
+  useEffect(() => {
+    if (ROLE !== "caregiver") return;
+    let reconectInternal;
 
-		if (!wsCaregiver) {
-			const newWs = new WebSocket(WS_URL);
+    const connectWebSocket = () => {
+      const newWs = new WebSocket(WS_URL);
 
-			newWs.onopen = () => {
-				newWs.send(
-					JSON.stringify({
-						type: 'register',
-						role: 'caregiver',
-						caregiverId : caregiverId,
-					})
-				);
-			};
+      newWs.onopen = () => {
+        newWs.send(
+          JSON.stringify({
+            type: "register",
+            role: "caregiver",
+            caregiverId: caregiverId,
+          })
+        );
+      };
 
-			newWs.onmessage = (event) => {
-				// Lógica para manejar mensajes recibidos
-				try {
-					const data = JSON.parse(event.data);
-					if (data.type === 'offers_update') {
-						if (lastProcessedMessage === data.type) return;
+      newWs.onmessage = (event) => {
+        // Lógica para manejar mensajes recibidos
+        try {
+          const data = JSON.parse(event.data);
+          if (data.type === "offers_update") {
+            if (lastProcessedMessage === data.type) return;
 
-						setLastProcessedMessage(data.type);
+            setLastProcessedMessage(data.type);
 
-						dispatch(actionGetOffersOwner());
-						dispatch(
-							setAlert({
-								message: 'Hay ofertas nuevas disponiples 😊',
-								type: 'success',
-							})
-						);
-						setLastProcessedMessage(null);
-					}
+            dispatch(actionGetOffersOwner());
+            dispatch(
+              setAlert({
+                message: "Hay ofertas nuevas disponiples 😊",
+                type: "success",
+              })
+            );
+            setLastProcessedMessage(null);
+          }
 
-					if(data.type === 'payment_complete'){
-						if (lastProcessedMessage === data.type) return;
-						setLastProcessedMessage(data.type);
-						dispatch(actionGetOffersOwner());
-						dispatch(
-							setAlert({
-								message: `El pago de ${data.petName} ha sido realizado por ${data.ownerName}`,
-								type: 'success',
-							})
-						);
-						setLastProcessedMessage(null);
-					}
-				} catch (error) {
-					console.log(error);
-				}
-			};
+          if (data.type === "payment_complete") {
+            if (lastProcessedMessage === data.type) return;
+            setLastProcessedMessage(data.type);
+            dispatch(actionGetOffersOwner());
+            dispatch(
+              setAlert({
+                message: `El pago de ${data.petName} ha sido realizado por ${data.ownerName}`,
+                type: "success",
+              })
+            );
+            setLastProcessedMessage(null);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
 
-			newWs.onclose = () => {
-				console.log('Connection closed');
-				dispatch(setWs(null)); // Reiniciar la conexión WebSocket si se cierra
-			};
+      newWs.onclose = () => {
+        console.log("Connection closed");
+        dispatch(setWs(null)); // Reiniciar la conexión WebSocket si se cierra
 
-			dispatch(setWs(newWs));
-		}
+        reconectInternal = setInterval(() => {
+          connectWebSocket();
+        }, 10000);
+      };
 
-		return () => {
-			if (wsCaregiver  && routerNames['landing'] === location.pathname ) {
-				wsCaregiver.close(); 
-			}
-		};
-	}, [ dispatch, ROLE, lastProcessedMessage, location.pathname, wsCaregiver, caregiverId]);
+      dispatch(setWs(newWs));
+    };
+    if (ROLE === "caregiver" && !wsCaregiver) {
+      connectWebSocket();
+    }
+    return () => {
+      if (reconectInternal) {
+        clearInterval(reconectInternal);
+      }
+      if (wsCaregiver && routerNames["landing"] === location.pathname) {
+        wsCaregiver.close();
+      }
+    };
+  }, [
+    dispatch,
+    ROLE,
+    lastProcessedMessage,
+    location.pathname,
+    wsCaregiver,
+    caregiverId,
+  ]);
 
-	const sendMessageCaregiver = (message) => {
-		if (wsCaregiver && wsCaregiver.readyState === WebSocket.OPEN) {
-			wsCaregiver.send(JSON.stringify(message));
-		} else {
-			console.error('WebSocket connection is not established or is closed');
-		}
-	};
+  const sendMessageCaregiver = (message) => {
+    if (wsCaregiver && wsCaregiver.readyState === WebSocket.OPEN) {
+      wsCaregiver.send(JSON.stringify(message));
+    } else {
+      console.error("WebSocket connection is not established or is closed");
+    }
+  };
 
-	return { sendMessageCaregiver, wsCaregiver };
+  return { sendMessageCaregiver, wsCaregiver };
 };
 
 export default useWsCaregiver;
--- client/src/common/utils/websocket/useWsOwner.jsx
+++ client/src/common/utils/websocket/useWsOwner.jsx
@@ -1,49 +1,50 @@
-import { WS_URL } from '@src/common/constants/api';
-import  { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setAlert } from '@src/common/store/slices/alertSlice';
-import { setWs } from '@src/common/store/slices/wsSlice';
-import routerNames from '@src/common/constants/routes';
-import { useLocation } from 'react-router-dom';
+import { WS_URL } from "@src/common/constants/api";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setAlert } from "@src/common/store/slices/alertSlice";
+import { setWs } from "@src/common/store/slices/wsSlice";
+import routerNames from "@src/common/constants/routes";
+import { useLocation } from "react-router-dom";
 
 const useWsOwner = () => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const location = useLocation();
 
   const [lastProcessedMessage, setLastProcessedMessage] = useState(null);
 
-  const ownerId = useSelector((state) => state.userReducer.user?.owner?.id)
-  const ROLE = useSelector((state) => state?.userReducer?.user?.role)
+  const ownerId = useSelector((state) => state.userReducer.user?.owner?.id);
+  const ROLE = useSelector((state) => state?.userReducer?.user?.role);
   const wsOwner = useSelector((state) => state.wsReducer.ws);
 
-	useEffect(() => {
-    if(ROLE !== 'owner') return;
+  useEffect(() => {
+    if (ROLE !== "owner") return;
+    let reconectInternal;
 
-		if (!wsOwner) {
-			const newWs = new WebSocket(WS_URL);
+    const connectWebSocket = () => {
+      const newWs = new WebSocket(WS_URL);
 
-			newWs.onopen = () => {
-				console.log('connected');
+      newWs.onopen = () => {
+        console.log("connected");
         newWs.send(
           JSON.stringify({
-            type: 'register',
-            role: 'owner',
-            ownerId : ownerId,
+            type: "register",
+            role: "owner",
+            ownerId: ownerId,
           })
         );
-			};
+      };
 
-			newWs.onmessage = (event) => {
-				// Lógica para manejar mensajes recibidos
+      newWs.onmessage = (event) => {
+        // Lógica para manejar mensajes recibidos
         try {
           const data = JSON.parse(event.data);
-          if (data.type === 'request_update' && data.ownerId === ownerId) {
+          if (data.type === "request_update" && data.ownerId === ownerId) {
             if (lastProcessedMessage === data.type) return;
             setLastProcessedMessage(data.type);
             dispatch(
               setAlert({
                 message: `¡Hay una nueva oferta para ${data.petName} de ${data.caregiverName}!`,
-                type: 'success',
+                type: "success",
               })
             );
           }
@@ -51,33 +52,49 @@ const useWsOwner = () => {
         } catch (error) {
           console.log(error);
         }
-			};
+      };
 
+      newWs.onclose = () => {
+        console.log("Connection closed");
+        dispatch(setWs(null)); // Reiniciar la conexión WebSocket si se cierra
 
-			newWs.onclose = () => {
-				console.log('Connection closed');
-				dispatch(setWs(null)); // Reiniciar la conexión WebSocket si se cierra
-			};
+        reconectInternal = setInterval(() => {
+          connectWebSocket();
+        }, 10000);
+      };
 
-			dispatch(setWs(newWs));
-		}
+      dispatch(setWs(newWs));
+    };
+    if (ROLE === "owner" && !wsOwner) {
+      connectWebSocket();
+    }
 
-		return () => {
-			if (wsOwner && routerNames['landing'] === location.pathname) {
-				wsOwner.close(); // Cerrar la conexión al desmontar el componente
-			}
-		};
-	}, [ dispatch, ROLE,lastProcessedMessage, location.pathname, ownerId, wsOwner]);
+    return () => {
+      if (reconectInternal) {
+        clearInterval(reconectInternal);
+      }
+      if (wsOwner && routerNames["landing"] === location.pathname) {
+        wsOwner.close(); // Cerrar la conexión al desmontar el componente
+      }
+    };
+  }, [
+    dispatch,
+    ROLE,
+    lastProcessedMessage,
+    location.pathname,
+    ownerId,
+    wsOwner,
+  ]);
 
   const sendMessageOwner = (message) => {
     if (wsOwner && wsOwner.readyState === WebSocket.OPEN) {
       wsOwner.send(JSON.stringify(message));
     } else {
-      console.error('WebSocket connection is not established or is closed');
+      console.error("WebSocket connection is not established or is closed");
     }
   };
 
-	return { sendMessageOwner, wsOwner };
+  return { sendMessageOwner, wsOwner };
 };
 
 export default useWsOwner;
