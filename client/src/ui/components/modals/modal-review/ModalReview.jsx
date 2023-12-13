72e872b4b8e1725b13ae7830b233946e0a138795
EseToni <falsigfx@gmail.com>
2023-12-13 13:32:15 +0100

    [UPDATE] FormReview ahora agarra la informacion correctamente de aquien le va a hacer la review

--- client/src/ui/components/modals/modal-review/ModalReview.jsx
+++ client/src/ui/components/modals/modal-review/ModalReview.jsx
@@ -1,11 +1,35 @@
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 import CardUser from "../../cards/card-user/CardUser";
 import FormReview from "../../forms/form-review/FormReview";
 import styles from "./styles.module.scss";
 import cross from "@icons/cross.svg";
+import { API_URL_CAREGIVERS, API_URL_OWNERS } from "@src/common/constants/api";
 
-const ModalReview = ({ reviewsData, role, closeModal, setNotification }) => {
-  const user = useSelector((state) => state.userReducer.user);
+const ModalReview = ({ reviewsData, closeModal, setNotification }) => {
+
+  const role = useSelector((state) => state.userReducer.user.role);
+  const [detailsUser, setDetailsUser] = useState({});
+
+  const getDetailsUser = async () => {
+    try {
+      const url =
+        role === "owner"
+          ? `${API_URL_CAREGIVERS}${reviewsData.caregiverId}`
+          : `${API_URL_OWNERS}/${reviewsData.ownerId}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setDetailsUser(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getDetailsUser();
+  }, []);
+
+
   return (
     <div className={styles.containerFlex}>
       <div className={styles.background} onClick={() => closeModal()}></div>
@@ -19,13 +43,19 @@ const ModalReview = ({ reviewsData, role, closeModal, setNotification }) => {
         <h3>Déjanos tu reseña</h3>
         <div className={styles.subContTwo}>
           <CardUser
-            name={user.name}
-            role={user.role}
-            address={user.address}
-            imgSrc={user.profilePicture}
+            name={detailsUser?.user?.name}
+            role={detailsUser?.user?.role}
+            address={detailsUser?.user?.address}
+            imgSrc={detailsUser?.user?.profilePicture}
+            rating={detailsUser?.rating}
           />
 
-          <FormReview className={styles.formReview} reviewsData={reviewsData} closeModal={closeModal} setNotification={setNotification}/>
+          <FormReview
+            className={styles.formReview}
+            reviewsData={reviewsData}
+            closeModal={closeModal}
+            setNotification={setNotification}
+          />
         </div>
       </div>
     </div>
